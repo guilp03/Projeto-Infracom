a47d738287789f68fb7815ceb701100e0803f7f9
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-07-27 15:56:36 -0300

    primeira entrega

---
+++ server_udp.py
@@ -0,0 +1,16 @@
+import socket
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind(('', 4433))
+print ("Servidor Pronto")
+try:
+    while True:
+        message, endereco_client = serverSocket.recvfrom(1024)
+        msg = message.decode()
+        serverSocket.sendto(msg.encode(),endereco_client)
+        if msg == "sair\n":
+            break
+except Exception as error:
+    print("Algo deu Errado")
+    print(error)
+    serverSocket.close()
