0d0a903facdc6cf4aaf68acbf3526aaa0bbf87e3
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-08-01 16:35:27 -0300

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -3,13 +3,14 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     while True:
-        file = input("file path: ")
-        with open(file, "rb") as file:
+        file_name = input("file path: ")
+        with open(file_name, "rb") as file:
             while True:
                 dados = file.read(1024)
                 if not dados:
                     break
-        client.sendto(dados, ("127.0.0.1", 4433))
+        data = file_name.encode() + b'|' + dados
+        client.sendto(data, ("127.0.0.1", 4433))
         client.sendto(b"", ("127.0.0.1", 4433))
         data, sender = client.recvfrom(1024)
         nome_arquivo, conteudo = data.split(b'|', 1)
