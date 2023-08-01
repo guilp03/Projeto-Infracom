73aaa7ae027084606895fad5852d26f79104ab10
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-08-01 16:18:17 -0300

    atualizacao

--- client_udp.py
+++ client_udp.py
@@ -3,14 +3,18 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     while True:
-        msg = input("Mensagem: ") + "\n"
-        client.sendto(msg.encode(), ("127.0.0.1", 4433))
+        file = input("file path: ")
+        with open(file, "rb") as file:
+            while True:
+                dados = file.read(1024)
+                if not dados:
+                    break
+        client.sendto(dados, ("127.0.0.1", 4433))
+        client.sendto(b"", ("127.0.0.1", 4433))
         data, sender = client.recvfrom(1024)
-        print(sender[0] + ": " + data.decode())
-        if data.decode() == "sair\n" or msg == "sair\n":
-            break
-
-    client.close()
+        nome_arquivo, conteudo = data.split(b'|', 1)
+        print(sender[0] + ": " + nome_arquivo)
+        client.close()
 except Exception as error:
     print("Algo deu Errado")
     print(error)
--- server_udp.py
+++ server_udp.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', 4433))
@@ -6,10 +7,17 @@ print ("Servidor Pronto")
 try:
     while True:
         message, endereco_client = serverSocket.recvfrom(1024)
-        msg = message.decode()
-        serverSocket.sendto(msg.encode(),endereco_client)
-        if msg == "sair\n":
-            break
+        nome_arquivo, conteudo = message.split(b'|', 1)
+        with open(nome_arquivo, 'wb') as file:
+            file.write(conteudo)
+        os.rename(nome_arquivo, "renomeado.txt")
+        with open(file, "rb") as file:
+            while True:
+                dados = file.read(1024)
+                if not dados:
+                    break
+        serverSocket.sendto(dados,endereco_client)
+        serverSocket.close()
 except Exception as error:
     print("Algo deu Errado")
     print(error)
