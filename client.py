e87b61471c0d8a984e034d2675f22fcc09a981bd
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-08-06 15:37:17 -0300

    cabou

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ client = socket(AF_INET, SOCK_DGRAM)
 try:
     file_name = str(input('digite o nome do arquivo: '))
     client.sendto(file_name.encode(), ('127.0.0.1', 7000))
-    path = './' + file_name
+    path = './arquivos/' + file_name
     file =  open(path, mode="rb")
     while True:
         data = file.read(1024)
@@ -16,10 +16,9 @@ except Exception as error:
     print("Algo deu Errado")
     print(error)  
 try:
-    with open("new_" + file_name, 'wb') as file:
+    with open("./arquivos/new_" + file_name, 'wb') as file:
         while True:
             data = client.recv(1024)
-            print(data)
             client.settimeout(2)
             if (not data):
                 break
