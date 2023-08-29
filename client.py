f26226de273c615f0d2c1e33861dcea772e17d8c
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-08-29 16:09:06 -0300

    deu tudo errado lesgooooo

--- client.py
+++ client.py
@@ -1,72 +1,27 @@
 from socket import *
-from struct import *
-ACK = 0
-SEQ = 0
-FIN = 0
+from pickle import *
 
-handshake_packet = Struct('I I')
-handshake_unpack = Struct('I I I')
+SEQ = 0
+
+server_adress = ("127.0.0.1", 4433)
 usuario = input("Escolha seu nome de usuario: ")
 client = socket(AF_INET, SOCK_DGRAM)
-unpack_len = 'I'
 #Handshake
 while True:
-    try:
-        pacote = (SEQ, 1)
-        pacote_handshake = handshake_packet.pack(*pacote)
-        client.sendto(pacote_handshake, ("127.0.0.1", 4433))
-        print("Tentando Conexão...")
-        client.settimeout(0.009)
-        data, sender = client.recvfrom(1024)
-        pacote_resposta = handshake_unpack.unpack(data)
-        if pacote_resposta[2] == 1 and pacote_resposta[1] == 1:
-            SEQ += 1
-            print("Conexão Estabelecida")
-            break
-    except timeout:
-            print("timeout ocorreu")
-            print("sistema tentará envio mais uma vez")
-            print("_______________________________________________")    
-            continue
-        
-while True:
-    try:
-        msg = input("Mensagem: ") + "\n"
-        len_mensagem = len(msg)
-        UDP_Packet_Data = Struct(f'I I I {len_mensagem}s')
-        mensagem = msg.encode()
-        pacote = (SEQ, ACK, FIN, len_mensagem, mensagem)
-        pacote_udp = UDP_Packet_Data.pack(*pacote)
-        print("Enviando...")
-    except Exception as error:
-        print("Algo deu Errado")
-        print(error)
-        client.close()
-        break
+    msg = input("Digite Sua Mensagem: ")
+    pacote = (SEQ, msg.encode())
+    pacote_serializado = dumps(pacote)
     while True:
         try:
-            client.sendto(pacote_udp, ("127.0.0.1", 4433))
-            print("Enviado!")
-            client.settimeout(0.009)
-            print("Esperando por ACK...")
+            client.sendto(pacote_serializado,server_adress)
+            client.settimeout(0.020)
             data, sender = client.recvfrom(1024)
-            unpack_size = unpack_len.unpack(data[2])
-            clientUnpacker = Struct(f'I I I {unpack_size}s')
-            pacote_resposta = clientUnpacker.unpack(data)
-            responseack = pacote_resposta[1]
-            if ACK != responseack:
-                print(pacote_resposta[0], pacote_resposta[1], pacote_resposta[2], pacote_resposta[3])
-                print("ACK incorreto")
-                continue
+            if data.decode() == SEQ:
+                if SEQ == 0:
+                    SEQ = 1
+                else:
+                    SEQ = 0
             break
         except timeout:
-            print("timeout ocorreu")
-            print("sistema tentará envio mais uma vez")
-            print("_______________________________________________")    
+            print("Times Estourou! Reenviando...")
             continue
-        
-    if ACK == 0:
-        ACK = 1
-    elif ACK == 1:
-        ACK = 0
-    SEQ += len_mensagem
--- server.py
+++ server.py
@@ -1,26 +1,10 @@
 from socket import *
-from struct import *
+from pickle import *
 
 ACK = 0
-SEQ = 0
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(("127.0.0.1", 4433))
 print ("Servidor Pronto")
-handshake_pack = ('I I I')
-unpack_len = 'I'
-
-while True:
-    data, sender = serverSocket.recvfrom(1024)
-
-    if unpack_len.unpack(data[0]) == 0 and unpack_len.unpack(data[1]) == 1:
-        #iniciar o handshake
-        pacote = (SEQ, 1, ACK)
-        handshake_packet = handshake_pack.pack(*pacote)
-        while True:
-            serverSocket.sendto(handshake_packet, sender)
-            print("Pedido de Conexão recebido!")
-            print("Confirmando...")
-            serverSocket.settimeout(0.009)
-            data, sender = serverSocket.recvfrom(1024)
+print("--------------------------------Iniciar Chat----------------------------------------")
 
