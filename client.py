4b8deb7d7861fe605bf7a70ec88840cd54b27a7d
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-08-29 06:27:53 -0300

    comecei a segunda entrega

--- client.py
+++ client.py
@@ -1,46 +1,50 @@
 from socket import *
-
-# Inicializando o socket, passando a versao do protocolo IP (IPv4) e o protocolo de transporte (UDP)
+from struct import *
+ack = 0
+seq = 0
+    
+usuario = input("Escolha seu nome de usuario: ")
 client = socket(AF_INET, SOCK_DGRAM)
-# Enviando os dados para o servidor
-try:
-    # Recebe do usuario o nome do arquivo a ser enviado
-    file_name = str(input('Digite o nome do arquivo: '))
-    # Envia o nome do arquivo que sera enviado (em bytes) para o servidor, passando a tupla (IP, porta) do socket do servidor
-    client.sendto(file_name.encode(), ('127.0.0.1', 7000))
-    # Cria uma variavel pra armazenar o caminho onde o arquivo que sera usado para o envio esta, incluindo tambem o nome dele
-    path = './arquivos/' + file_name
-    # Abre o arquivo com permissao de leitura binaria
-    file =  open(path, mode="rb")
-    # Envia o arquivo enquanto houver pacotes a serem enviados
+unpack_len = 'I'
+while True:
+    try:
+        msg = input("Mensagem: ") + "\n"
+        len_mensagem = len(msg)
+        UDP_Packet_Data = Struct(f'I I I {len_mensagem}s')
+        mensagem = msg.encode()
+        pacote = (ack, seq, len_mensagem, mensagem)
+        pacote_udp = UDP_Packet_Data.pack(*pacote)
+        client.sendto(pacote_udp, ("127.0.0.1", 4433))
+        print("Enviando...")
+    except Exception as error:
+        print("Algo deu Errado")
+        print(error)
+        client.close()
+        break
     while True:
-        # Divide o arquivo em pacotes de no maximo 1024 bytes
-        data = file.read(1024)
-        # Quando acabarem os pacotes, encerra o while
-        if (not data):
-            break
-        # Envia os pacotes para o servidor, passando os pacotes e a tupla (IP, porta) do socket do servidor
-        client.sendto(data, ('127.0.0.1', 7000))
-    # Encerra a edicao do arquivo
-    file.close()
-except Exception as error:
-    print("Algo deu Errado")
-    print(error)
-# Recebendo de volta o arquivo do servidor
-try:
-    with open("./arquivos/new_" + file_name, 'wb') as file:
-        while True:
-             # Divide o arquivo em pacotes de no maximo 1024 bytes
-            data = client.recv(1024)
-            # Define o tempo maximo de execucao do socket do cliente, enquanto esta recebendo de volta o arquivo do servidor (2 segundos)
-            client.settimeout(2)
-            # Quando acabarem os pacotes, encerra o while
-            if (not data):
-                break
-            # Escreve os bytes do arquivo recebido no novo arquivo
-            file.write(data)
-except Exception as error:
-    print("Algo deu Errado")
-    print(error)
-    # Caso encontre um erro, garante que o socket sera fechado
-    client.close()
+        try:
+            client.sendto(pacote_udp, ("127.0.0.1", 4433))
+            ack_momento = ack
+            print("Enviado!")
+            client.settimeout(0.009)
+            print("Esperando por ACK...")
+            data, sender = client.recvfrom(1024)
+            unpack_size = unpack(data[2])
+            clientUnpacker = Struct(f'I I I {unpack_size}s')
+            pacote_resposta = clientUnpacker(data)
+            responseack = pacote_resposta[0]
+            #comparar o ack
+            #arrumar o timeout
+            if not data:
+                while not data:
+                    client.sendto(msg.encode(), ("127.0.0.1", 4433))
+                    #timeout
+                    data, sender = client.recvfrom(1024)            
+
+                
+                
+        except Exception as error:
+            print("Algo deu Errado")
+            print(error)
+            client.close()
+            break        
--- server.py
+++ server.py
@@ -1,54 +1,17 @@
 from socket import *
 
-# Inicializando o Socket, passando a versao do protocolo IP (IPv4) e o protocolo de transporte (UDP)
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Define o socket do servidor como tendo o IP do localhost ('') e a porta 7000
-serverSocket.bind(('', 7000))
-print ("O servidor está pronto")
-# Recebendo os dados do cliente
+serverSocket.bind(('', 4433))
+print ("Servidor Pronto")
+
 try:
-    # Recebe o nome do arquivo que sera recebido do cliente (em bytes) e a tupla (IP, Porta) do socket do cliente (em pacotes de até 1024 bytes)
-    arquivo, endereco_client = serverSocket.recvfrom(1024)
-    print("O cliente possui socket: ", endereco_client)
-    # Resgata a string do nome do arquivo dos bytes recebidos
-    filename = arquivo.decode()
-    print("O arquivo recebido será: " + filename)
-    # Cria uma variavel pra armazenar o caminho onde o arquivo que sera recebido esta, incluindo tambem o nome dele
-    path = './' + filename
-    # Abre o arquivo com permissao de escrita binaria
-    file = open(path, mode="wb")
-    # Recebe o arquivo enquanto houver pacotes a serem recebidos
     while True:
-        # Recebe o arquivo de fato (em bytes) e a tupla (IP, Porta) do socket do cliente em pacotes de ate 1024 bytes
-        data, client_adress  = serverSocket.recvfrom(1024)
-        # Define o tempo maximo de execucao do socket do servidor, enquanto esta recebendo o arquivo do cliente (2 segundos)
-        serverSocket.settimeout(2)
-        # Quando acabarem os pacotes, encerra o while
-        if (not data):
+        message, endereco_client = serverSocket.recvfrom(1024)
+        msg = message.decode()
+        serverSocket.sendto(msg.encode(),endereco_client)
+        if msg == "sair\n":
             break
-        # Escreve os bytes do arquivo recebido no arquivo que teve seu conteudo apagado
-        file.write(data)
-    # Encerra a edicao do arquivo
-    file.close()
 except Exception as error:
     print("Algo deu Errado")
     print(error)
-# Enviando de volta o arquivo para o cliente 
-try:
-    with open(arquivo, 'rb') as file:
-        # Envia de volta para o cliente o arquivo antes recebido, enquanto houver pacotes a serem enviados
-        while True:
-            # Divide o arquivo em pacotes de no maximo 1024 bytes
-            data = file.read(1024)
-            # Define o tempo maximo de execucao do socket do servidor, enquanto esta enviando de volta o arquivo para o cliente (2 segundos)
-            serverSocket.settimeout(2)
-            # Quando acabarem os pacotes, encerra o while
-            if (not data):
-                break
-            # Envia para o cliente, passando os pacotes e a tupla (IP, porta) do seu socket, resgatado quando recebeu o nome do arquivo
-            serverSocket.sendto(data, endereco_client)
-except Exception as error:
-    print("Algo deu Errado")
-    print(error)
-    # Caso encontre um erro, garante que o socket sera fechado
-    serverSocket.close()
+    serverSocket.close() 
